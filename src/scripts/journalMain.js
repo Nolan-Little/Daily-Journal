472080b4ae1a4e74cb4f9e4d36e06542366827ae
Nolan-Little <little.nolan@gmail.com>
2018-11-06 09:26:44 -0600

    complete dj 5. added form validation and added form to page dynamically

--- src/scripts/journalMain.js
+++ src/scripts/journalMain.js
@@ -1,10 +1,9 @@
-// add event listener that calls previous methods and targets "print all entries button" to print all entries 
+// add event listener that calls previous methods and targets "print all entries button" to print all entries
 // in the database.json file and renders them to the page.
 
 
 let printButton = document.querySelector("#journalPrint")
 printButton.addEventListener("click", () => {
   entryFetcher.fetchEntries()
-  .then((readyToPrint) => printOnClick(readyToPrint))
-  })
-  
+    .then((readyToPrint) => printOnClick(readyToPrint))
+})
